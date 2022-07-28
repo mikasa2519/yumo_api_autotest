d5abacd1c77c3f51457723bc617297f58c379725
zpliu2 <425204185@qq.com>
2022-07-28 20:56:39 +0800

    测试新电脑

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ from commons.yaml_util import read_extract_yaml
 if __name__ == '__main__':
     # pytest.main()
 
-    url = "https://api.weixin.qq.com/cgi-bin/tags/get?access_token=${access_token}&abc=${abc}"
+    url = "https://api.weixin.qq.com/cgi-bin/tags/get?access_token=${access_token}&expires_in=${expires_in}"
     for a in range(1, url.count("${")+1):
         if "${" and "}" in url:
             start_index = url.index("${")
             end_index = url.index("}", start_index)
             old_value = url[start_index:end_index + 1]
             new_value = read_extract_yaml("extract.yaml", old_value[2:-1])
-            new_url = url.replace(old_value, new_value)
+            new_url = url.replace(old_value, str(new_value))
             url = new_url
             print(url)
--- testcases/user_manager/test_wechat.py
+++ testcases/user_manager/test_wechat.py
@@ -4,7 +4,7 @@ import re
 import pytest
 
 from commons.request_util import RequestUtil
-from commons.yaml_util import write_yaml, read_extract_yaml, read_testcase_yaml
+from commons.yaml_util import read_extract_yaml, read_testcase_yaml
 
 
 class TestApi:
@@ -43,16 +43,16 @@ class TestApi:
     #     result = res.json()
     #     print(result, type(result))
     #
-    # @pytest.mark.parametrize("caseinfo", read_testcase_yaml("testcases/user_manager/file_upload.yaml"))
-    # def test_file_upload(self, caseinfo):
-    #     method = caseinfo["request"]["method"]
-    #     urls = caseinfo["request"]["url"]
-    #     params = caseinfo["request"]["params"]
-    #     params["access_token"] = read_extract_yaml("extract.yaml", "access_token")
-    #     datas = caseinfo["request"]["files"]
-    #     datas["media"] = open(datas["media"], "rb")
-    #     res = RequestUtil().send_request(method="post", url=urls, files=datas, params=params)
-    #     result = res.json()
-    #     print(result, type(result))
+    @pytest.mark.parametrize("caseinfo", read_testcase_yaml("testcases/user_manager/file_upload.yaml"))
+    def test_file_upload(self, caseinfo):
+        methods = caseinfo["request"]["method"]
+        urls = caseinfo["request"]["url"]
+        params = caseinfo["request"]["params"]
+        params["access_token"] = read_extract_yaml("extract.yaml", "access_token")
+        datas = caseinfo["request"]["files"]
+        datas["media"] = open(datas["media"], "rb")
+        res = RequestUtil().send_request(method=methods, url=urls, files=datas, params=params)
+        result = res.json()
+        print(result, type(result))
 
 
